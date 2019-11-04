e4f48c35d2b1f52a797a1a8f7db8b2741fbbde28
Felix hennig <mail@felixhennig.com>
2019-11-04 01:30:27 +0000

    Added script to generate gifs from images.

---
+++ gifgen.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+palette="/tmp/palette.png"
+
+filters="scale=360:-1:flags=lanczos"
+
+ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -r 10 -y $2
